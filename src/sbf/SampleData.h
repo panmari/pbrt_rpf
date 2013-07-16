4a11ab010aab810fc3b7f1b60664e2ec7538d804
panmari <smmuzi@gmail.com>
2013-07-16 12:05:58 +0200

    removed unused methods from SampleData

--- src/sbf/SampleData.h
+++ src/sbf/SampleData.h
@@ -45,12 +45,8 @@ struct SampleData {
 	static int getRandomParamsOffset()		{ return 20; }
 	static int getRandomParametersSize()	{ return 5; }
 	static int getLastNormalizedOffset()	{ return 25; } //this should mark the last used parameter
-	void   operator+=(const SampleData& s)	{ for(int i=0;i<getSize();i++) (*this)[i] += s[i]; }
-	void   divide(int s)					{ for(int i=0;i<getSize();i++) (*this)[i] /= float(s); }
 	float& operator[](int i)				{ return ((float*)this)[i]; }
 	const float& operator[](int i) const	{ return ((float*)this)[i]; }
-	float sum() const						{ float s=0; for(int i=0;i<getSize();i++) s+=(*this)[i]; return s; }
-	float avg() const						{ return sum()/getSize(); }
 };
 
 #endif //SAMPLE_DATA_H
